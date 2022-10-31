5dd0fe5cd1a65fe523b875bda7815d52a83ebd3c
Ingvar Derešivski <inde999@gmail.com>
2022-10-31 09:17:35 +0200

    CourseCRUD

--- ContosoUniversityTARgv21/ContosoUniversityTARgv21/Controllers/CoursesController.cs
+++ ContosoUniversityTARgv21/ContosoUniversityTARgv21/Controllers/CoursesController.cs
@@ -1,7 +1,10 @@
 ﻿using ContosoUniversityTARgv21.Data;
+using ContosoUniversityTARgv21.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace ContosoUniversityTARgv21.Controllers
 {
@@ -27,6 +30,92 @@ namespace ContosoUniversityTARgv21.Controllers
                 .ToListAsync();
 
             return View(courses);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            PopulateDepartmentDropDownList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateDepartmentDropDownList();
+            return View(course);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            PopulateDepartmentDropDownList();
+            return View(course);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Post(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseToUpdate = await _context.Courses
+                .FirstOrDefaultAsync(c => c.CourseId == id);
+
+            if (await TryUpdateModelAsync<Course>
+                (courseToUpdate, "", 
+                c => c.Credits, c => c.DepartmentId, c => c.Title)
+                )
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem presists, " +
+                        "see your system administrator");
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateDepartmentDropDownList(courseToUpdate.DepartmentId);
+            return View(courseToUpdate);
+        }
+
+        private void PopulateDepartmentDropDownList(object selectedDepartment = null)
+        {
+            var departmentsQuery = from d in _context.Departments
+                                   orderby d.Name
+                                   select d;
+            ViewBag.DepartmentId = new SelectList(departmentsQuery.AsNoTracking(),
+                "DepartmentId", "Name", selectedDepartment);
         }
     }
 }
